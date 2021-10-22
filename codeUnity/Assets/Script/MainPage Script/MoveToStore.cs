6969d27818c6085e6580f5490b6d18eb792c3c72
NNHHLong <57785199+NNHHLong@users.noreply.github.com>
2021-10-23 00:15:48 +0700

    Build Setting All Scence
    Best Movement Ever

---
+++ codeUnity/Assets/Script/MainPage Script/MoveToStore.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MoveToStore : MonoBehaviour
+{
+    public void storeOpening()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+    }
+
+    public void backtoMainPage()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+}
