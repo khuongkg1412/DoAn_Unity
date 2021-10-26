5b725e469d527c5ac001b62536909ccc09781d19
Khuong Nguyen <khuongkg1412@gmail.com>
2021-10-26 23:50:33 +0700

    done open panel mess

--- codeUnity/Assets/Script/Change Panel/changePanel.cs
+++ codeUnity/Assets/Script/Change Panel/changePanel.cs
@@ -4,8 +4,19 @@ using UnityEngine;
 
 public class changePanel : MonoBehaviour
 {
+    public GameObject chat_popup;
 
-
+    public void openChatPopup()
+    {
+        if (chat_popup != null)
+        {
+            chat_popup.SetActive(true);
+        }
+    }
+    public void closeChatPopup()
+    {
+            chat_popup.SetActive(false);
+    }
 
 
 
