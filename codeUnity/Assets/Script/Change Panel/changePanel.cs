0f685d405c8815670ebb8d1b3792165413910362
Khuong Nguyen <khuongkg1412@gmail.com>
2021-10-26 22:17:23 +0700

    changePanel script

--- codeUnity/Assets/Script/Change Panel/changePanel.cs
+++ codeUnity/Assets/Script/Change Panel/changePanel.cs
@@ -4,15 +4,9 @@ using UnityEngine;
 
 public class changePanel : MonoBehaviour
 {
-    public GameObject chat_popup;
+    
 
-    public void openChatPopup(){
-
-        if (chat_popup != null)
-        {
-            chat_popup.
-        }
-    }
+    
 
 
 }
