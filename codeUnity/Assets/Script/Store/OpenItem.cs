870520a3ee5b4137a6bffa81882f4e41f7df9c5f
NNHHLong <57785199+NNHHLong@users.noreply.github.com>
2021-10-30 07:08:12 +0700

    Achie + Store

--- codeUnity/Assets/Script/Store/OpenItem.cs
+++ codeUnity/Assets/Script/Store/OpenItem.cs
@@ -6,17 +6,25 @@ public class OpenItem : MonoBehaviour
 {
     public GameObject blurBG;
     public GameObject pannel;
-    public void OpenPannelItem(){
+    public GameObject pannelChest;
+    public void OpenPannelItem(int i){
         if(pannel != null){
+            if(i>0){
+            blurBG.SetActive(true);
+            pannel.SetActive(false);
+            pannelChest.SetActive(true);
+            }else{
             blurBG.SetActive(true);
             pannel.SetActive(true);
+            pannelChest.SetActive(false);
+            }
+           
         }
     }
 
     public void ClosedPannelItem(){
         if(pannel != null){
             blurBG.SetActive(false);
-            pannel.SetActive(false);
         }
     }
     // Start is called before the first frame update
