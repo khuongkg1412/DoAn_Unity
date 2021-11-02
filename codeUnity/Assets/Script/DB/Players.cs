be1a6e712a8e0c1c02a5dd7ce9962b39a9c89e48
Khuong Nguyen <khuongkg1412@gmail.com>
2021-11-02 22:11:45 +0700

    load playerName complete

---
+++ codeUnity/Assets/Script/DB/Players.cs
@@ -0,0 +1,14 @@
+using System;
+[Serializable]
+public class Players
+{
+    public string playerName;
+
+    public Players()
+    {
+    }
+    public Players(string playerName)
+    {
+        this.playerName = playerName;
+    }
+}
