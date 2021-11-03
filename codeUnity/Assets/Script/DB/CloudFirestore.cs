16e94b32ea62692541672f3587b1aaa16f9e95f8
Khuong Nguyen <khuongkg1412@gmail.com>
2021-11-03 22:43:58 +0700

    add package

--- codeUnity/Assets/Script/DB/CloudFirestore.cs
+++ codeUnity/Assets/Script/DB/CloudFirestore.cs
@@ -1,7 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using Firebase.Firestore;
+using Firebase.Extensions;
 public class CloudFirestore : MonoBehaviour
 {
     // Start is called before the first frame update
