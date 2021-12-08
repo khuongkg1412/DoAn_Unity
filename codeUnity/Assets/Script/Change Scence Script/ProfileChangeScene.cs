8cde0e734d0837ffae4ddf251921d2ec291b8e17
Khuong Nguyen <khuongkg1412@gmail.com>
2021-12-08 22:44:23 +0700

    edit index scene

--- codeUnity/Assets/Script/Change Scence Script/ProfileChangeScene.cs
+++ codeUnity/Assets/Script/Change Scence Script/ProfileChangeScene.cs
@@ -5,22 +5,20 @@ using UnityEngine.SceneManagement;
 
 public class ProfileChangeScene : MonoBehaviour
 {
-    
     // Start is called before the first frame update
     public void SwitchFriendTab()
     {
-         SceneManager.LoadScene("Friend list");
-        
+        SceneManager.LoadScene("Friend list");
     }
 
     // Update is called once per frame
     public void SwitchProfileTab()
     {
-         SceneManager.LoadScene("Main screen");
+        SceneManager.LoadScene("Main screen");
     }
 
     public void backtoMainPage()
     {
-        SceneManager.LoadScene(0);
+        SceneManager.LoadScene(1);
     }
 }
