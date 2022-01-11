df2f2b8e40692c9f6bf365fb15501c7b747c0597
NNHHLong <57785199+NNHHLong@users.noreply.github.com>
2022-01-12 02:43:15 +0700

    QuantityControll

--- codeUnity/Assets/Script/Store/LoadingItem.cs
+++ codeUnity/Assets/Script/Store/LoadingItem.cs
@@ -10,28 +10,62 @@ public class LoadingItem : MonoBehaviour
     //Text content for name, type and description
     private TMPro.TMP_Text nameItem, type, description;
     //Get Diamond, coin
-    GameObject diamond, coin;
-
+    [SerializeField] GameObject diamondItem, coinItem;
+    [SerializeField] GameObject diamondChest, coinChest;
     RawImage dataImage;
-
+    enum QuantityButton
+    {
+        Plus = 0,
+        Minus = 1
+    }
+    [SerializeField] TMPro.TMP_Text quantityText;
+    public void quantityControll(int input)
+    {
+        int currentQuantity = int.Parse(quantityText.text);
+        if (input == (int)QuantityButton.Plus)
+        {
+            if (currentQuantity >= 0 && currentQuantity < 99)
+            {
+                currentQuantity += 1;
+                quantityText.text = currentQuantity.ToString();
+            }
+            else
+            {
+                Debug.LogError("Cannot Increase More");
+            }
+        }
+        else
+        {
+            if (currentQuantity > 0 && currentQuantity <= 99)
+            {
+                currentQuantity -= 1;
+                quantityText.text = currentQuantity.ToString();
+            }
+            else
+            {
+                Debug.LogError("Cannot Increase More");
+            }
+        }
+    }
     //Instantiate for all objects in the pannel
     void settingObject()
     {
-        nameItem = transform.GetChild(1).GetComponent<TMPro.TMP_Text>();
-        type = transform.GetChild(2).GetComponent<TMPro.TMP_Text>();
-        description = transform.GetChild(3).GetComponent<TMPro.TMP_Text>();
-        dataImage = transform.GetChild(0).gameObject.transform.GetChild(0).GetComponent<RawImage>();
-        diamond = transform.GetChild(4).gameObject;
-        coin = transform.GetChild(5).gameObject;
+        /*GeneralInform part*/
+        nameItem = transform.GetChild(2).transform.GetChild(0).GetComponent<TMPro.TMP_Text>();
+        type = transform.GetChild(2).transform.GetChild(1).GetComponent<TMPro.TMP_Text>();
+        description = transform.GetChild(2).transform.GetChild(2).GetComponent<TMPro.TMP_Text>();
+        /*ImagePannel part*/
+        dataImage = transform.GetChild(1).gameObject.transform.GetChild(0).GetComponent<RawImage>();
     }
     //Instantiate for all objects in the pannel but for chest only
     void settingObjectForChest()
     {
-        nameItem = transform.GetChild(1).GetComponent<TMPro.TMP_Text>();
-        description = transform.GetChild(3).GetComponent<TMPro.TMP_Text>();
-        dataImage = transform.GetChild(0).gameObject.transform.GetChild(0).GetComponent<RawImage>();
-        diamond = transform.GetChild(4).gameObject;
-        coin = transform.GetChild(5).gameObject;
+        /*GeneralInform part*/
+        nameItem = transform.GetChild(2).transform.GetChild(0).GetComponent<TMPro.TMP_Text>();
+        type = transform.GetChild(2).transform.GetChild(1).GetComponent<TMPro.TMP_Text>();
+        description = transform.GetChild(2).transform.GetChild(2).GetComponent<TMPro.TMP_Text>();
+        /*ImagePannel part*/
+        dataImage = transform.GetChild(1).gameObject.transform.GetChild(0).GetComponent<RawImage>();
     }
     //Set data for each objects
     void dataforItem()
@@ -49,19 +83,20 @@ public class LoadingItem : MonoBehaviour
         //Load data from Resource folders
         dataImage.texture = loadingImageFromFilePath(dataItem.image_Item);
         dataImage.SetNativeSize(); //Set native size for image
-        diamond.transform.GetChild(2).GetComponent<TMPro.TMP_Text>().text = dataItem.concurrency.Diamond.ToString();
-        coin.transform.GetChild(2).GetComponent<TMPro.TMP_Text>().text = dataItem.concurrency.Coin.ToString();
+        diamondItem.transform.GetChild(2).GetComponent<TMPro.TMP_Text>().text = dataItem.concurrency.Diamond.ToString();
+        coinItem.transform.GetChild(2).GetComponent<TMPro.TMP_Text>().text = dataItem.concurrency.Coin.ToString();
     }
     //Set data for each objects but for chest only
     void dataforItemChest()
     {
         nameItem.text = dataItem.name_Item;
         description.text = dataItem.description_Item;
+        type.text = "Chest";
         //Load data from Resource folders
         dataImage.texture = loadingImageFromFilePath(dataItem.image_Item);
         dataImage.SetNativeSize();//Set native size for image
-        diamond.transform.GetChild(2).GetComponent<TMPro.TMP_Text>().text = dataItem.concurrency.Diamond.ToString();
-        coin.transform.GetChild(2).GetComponent<TMPro.TMP_Text>().text = dataItem.concurrency.Coin.ToString();
+        diamondChest.transform.GetChild(2).GetComponent<TMPro.TMP_Text>().text = dataItem.concurrency.Diamond.ToString();
+        coinChest.transform.GetChild(2).GetComponent<TMPro.TMP_Text>().text = dataItem.concurrency.Coin.ToString();
     }
     /*
     This method is use to determine what type of item is, and what function would be call to process that type
