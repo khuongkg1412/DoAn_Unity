e0ceff75e0c839e5edad7d103c5d77b8acce5d36
Nguyen Van Khuong <khuongkg1412@gmail.com>
2022-01-13 22:37:01 +0700

    fix verticalLayout

---
+++ codeUnity/Assets/Script/LeaderBoard/LoadingLeaderboard.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadingLeaderboard : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] GameObject topImg, avtImg, namePlayer, mapPlayer, levelPlayer, flagImg;
+
+
+}
