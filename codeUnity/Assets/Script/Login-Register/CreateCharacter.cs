0daef207067970d5da46f8ff3bc44ec0b64eab9b
HaThucHuynhDuy <52154699+davidduy@users.noreply.github.com>
2022-04-11 16:48:15 +0700

    xoa bien level

--- codeUnity/Assets/Script/Login-Register/CreateCharacter.cs
+++ codeUnity/Assets/Script/Login-Register/CreateCharacter.cs
@@ -113,7 +113,6 @@ public class CreateCharacter : MonoBehaviour
         Inventory_Player Buff = new Inventory_Player()
         {
             quantiy = 1,
-            level = 0
         };
 
         Friend_Player friend_Player = new Friend_Player()
